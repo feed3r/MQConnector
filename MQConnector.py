10af6b638f880344a105cb921da07a9d94cc47ee
feed3r <feeder81@gmail.com>
2014-07-07 11:10:55 +0200

    Refactor
    Some refactoring on Connector Object

--- MQConnector.py
+++ MQConnector.py
@@ -4,28 +4,28 @@ import pymqi
 import MQMessage
 
 class mqConnector:
-    def __init__(self, queue_manager, channel, host, port):
+
+    def __init__(self, queue_manager, channel, host, port, *args):
         self.queue_manager_name = queue_manager
         self.channel_name = channel
-        self.host = host
-        self.port = port
         self.queues = {}
+        self.queues_list = []
         self.message_data = {}
-
         self.conn_info = "%s(%s)" % (host, port)
+        for queue in args:
+            self.queues_list.append(queue)
+
+    def _add_queue(self, queue_name):
+        queue = pymqi.Queue(self.manager, queue_name)
+        self.queues[queue_name] = queue
 
     def connect(self):
         self.manager = pymqi.connect(self.queue_manager, self.channel, self.conn_info)
-
-    def set_queue(self, queue_name):
-        queue = pymqi.Queue(self.manager_obj, queue_name)
-        self.queue_dict[queue_name] = queue
-
-    def get_queue(self, queue_name):
-        return self.queue_dict[queue_name]
+        for queue in self.queues_list:
+            self._add_queue(queue)
 
     def send_message(self, queue_name, message):
-        self.queue_dict[queue_name].put(message.message_string)
+        self.queues[queue_name].put(message.message_string)
 
     def receive_message(self, queue_name):
         message = MQMessage()
@@ -33,15 +33,15 @@ class mqConnector:
         message.reset()
         return message
 
-    def close_queue(self, queue_name):
-        queue = self.queue_dict[queue_name]
+    def _close_queue(self, queue):
         queue.close()
         queue.disconnect()
 
     def close_all_queues(self):
-        for queue in self.queue_dict:
-            queue.close()
-            queue.disconnect()
+        for queue_name, queue in self.queues.items():
+            print("Closing Queue " + queue_name)
+            self._close_queue(queue)
+
 
 
 
